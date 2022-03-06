4c38f84ad47db595d97d229d56d389570d3e6ae5
Anush2303 <74965306+Anush2303@users.noreply.github.com>
2022-03-06 17:28:52 +0530

    Update Homepage.js

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,28 +1,29 @@
 import React from "react";
 import '../css/Homepage.css';
 import Navbar from './Navbar';
-import Preloader from "./Preloader";
-import 'animate.css';
+import self from "../images/self.jpeg";
+
 function Homepage(){
 return(
 <div className="header">
-  <div className="head-1"></div>
-  <div className="head-2"></div>
-  <div className="head-3"></div>
-  <div className="head-4"></div>
-  <Navbar />
-  <Preloader/>
-    <div className="box head animate__animated animate__fadeIn">
-        <h2>ANUSH GUPTA</h2>
-          <div className="row knowmebtn animate__animated animate__zoomIn" style={{textAlign:'center'}}>
-            <div className="wrap" style={{textAlign:'center'}}>
-              <form action="/about">
-              <button className="glow-on-hover" style={{color:'white'}}>KNOW ME {<i class="fas fa-code"></i>}</button>
-              </form>
-            </div>
-          </div>
-        </div>
-    </div>
+  <div className="head-1">
+   <Navbar />
+   <div className="boxc row">
+     <div className="col-12 col-md-12 col-lg-8">
+            <p className="sub">HELLO,WORLD.</p>
+            <p className="banner">I'M ANUSH GUPTA</p>
+            <h2 style={{color:"yellow",marginLeft:"5%",fontWeight:"bold"}}>ANUSH GUPTA</h2>     
+            <p className="banner_">#while(!(<span style={{color:"yellow"}}>succeed</span>=try()))</p>
+             <form action="/about">
+             <button className="animate__animated animate__zoomIn glow-on-hover block" style={{color:'white',marginLeft:"5%"}}>KNOW ME MORE{<i class="fas fa-code"></i>}</button>    
+             </form>
+     </div>
+     <div className="col-12 col-md-12 col-lg-4">
+            <img className="imgggg" src={self} alt="img"/>
+     </div>
+   </div>
+  </div>
+</div>
 );
 }
 export default Homepage;
