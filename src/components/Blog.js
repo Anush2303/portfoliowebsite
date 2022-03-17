01b025811267074a52c451d643ebe4c031038b7b
Anush2303 <74965306+Anush2303@users.noreply.github.com>
2022-03-17 15:24:15 +0530

    Update Blog.js

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -28,7 +28,7 @@ function Blog(){
   return(
         <div className="blog">
            <div className="rowimg" style={{paddingBottom:'7px'}}>
-              <p className='titleanimate_ animate__animated animate__zoomIn khead'><i class="page fab fa-blogger-b"></i>Blog App</p>
+              <p className='titleanimate_ animate__animated animate__zoomIn khead'><i class="page fab fa-blogger-b"></i>Live Journal</p>
               <a href="/link"><i class="linkbtn fas fa-code-branch fa-4x"></i></a>
             </div>
            <div className="rowimgb animate__animated animate__zoomIn">
@@ -43,14 +43,16 @@ function Blog(){
             <div className="__column" style={{marginLeft:'15px',marginRight:'10px'}}>
             <div class="card animate__animated animate__zoomIn" onMouseEnter={handlechange} onMouseLeave={change} style={_styles}>
               <div class="card-body">
-                <h3 class="card-title desc"style={{paddingBottom:'2px'}}>It is an online blog website where you can compose and post your daily journals and blogs and also have a read about other people's views and messages.</h3>
+                <h3 class="card-title desc"style={{paddingBottom:'2px'}}>This is a platform where you can read latest news that is catching attention around the globe.Also you can post your views and blogs so that it basically forms a community where people can come, read the latest news and express their views on that subject or any other personal blogs!</h3>
                 <div class="card-text">
                 <h3 class="" style={styles}>FEATURES</h3>
-                <h5>COMPOSE AND POST BLOG <i class="fas fa-user-plus icons" style={styles}></i></h5>
+                <h5>READ LATEST NEWS AROUND THE GLOBE<i class="fa-solid fa-globe" style={styles}></i></h5>
+                <h5>COMMENT AND POST ON THE NEWS<i class="far fa-comments icons" style={styles}></i></h5>
+                <h5>COMPOSE AND POST PERSONAL BLOGS<i class="fas fa-user-plus icons" style={styles}></i></h5>
                 <h5>VIEW OTHER INTERESTING BLOGS <i class="far fa-comments icons" style={styles}></i></h5>
                 <h3 class="" style={styles}>TECH USED</h3>
                 <h5>HTML | CSS | NODE | MONGODB | BOOTSTRAP</h5>
-                <h3 class="" style={styles}>PROJECT LINK  <a class="btn-outline-light btn-floating" href="https://github.com/Anush2303/BLOG_APP" role="button" style={{borderRadius:'100%'}}><i
+                <h3 class="" style={styles}>PROJECT LINK  <a class="btn-outline-light btn-floating" href="https://github.com/Anush2303/live_journal" role="button" style={{borderRadius:'100%'}}><i
                 class="fab fa-github fa-2x"></i></a></h3>
             </div>
         </div>
